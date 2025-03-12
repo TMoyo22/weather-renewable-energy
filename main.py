ea5f6678978cf3454ab89b72afec8784c0aae85e
Tatenda Moyo <131980229+TMoyo22@users.noreply.github.com>
2025-03-12 15:07:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+df = pd.read_csv(r"C:\Users\tatem\Desktop\Class\Dataset\Renewable.csv")
+
+print(df.head(10))
+print(df.tail(10))
